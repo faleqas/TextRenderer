953302d8ff76d579d42a2c21a62a31c95f7689d3
flex <omareltayeb529@gmail.com>
2024-09-12 09:38:32 +0300

    Reading main file header

--- source/main.c
+++ source/main.c
@@ -7,17 +7,46 @@
 static bool running = true;
 
 
+void load_ttf(const char* path);
 void main_loop();
 
 
-int main(void)
+int main(int argc, char** argv)
 {
 	SDL_Init(0);
 
+	load_ttf("Minecraft.ttf");
 	main_loop();
 
 	SDL_Quit();
 	return 0;
+}
+
+
+void load_ttf(const char* path)
+{
+	FILE* fp;
+	fopen_s(&fp, path, "rb");
+	if (!fp) return;
+
+	_getw(fp); //sfnt_version
+
+	uint16_t num_tables;
+	fread(&num_tables, sizeof(uint16_t), 1, fp);
+
+	fgetwc(fp); //search range
+	fgetwc(fp); //entry selector
+	fgetwc(fp); //range shift
+
+	printf("NUM TABLES: %hu\n", num_tables);
+
+	//read tables
+	for (int i = 0; i < num_tables; i++)
+	{
+
+	}
+
+	fclose(fp);
 }
 
 
@@ -26,7 +55,7 @@ void main_loop()
 	SDL_Window* window = SDL_CreateWindow("TextRenderer", 100, 100, 800, 600, 0);
 	SDL_Surface* bitmap = SDL_GetWindowSurface(window);
 	unsigned char* pixels = bitmap->pixels;
-	size_t bitmap_size = (bitmap->w * bitmap->h) * bitmap->format->BytesPerPixel;
+	const size_t bitmap_size = (bitmap->w * bitmap->h) * bitmap->format->BytesPerPixel;
 
 	SDL_Event e;
 	while (running)
@@ -41,9 +70,10 @@ void main_loop()
 
 		for (int i = 0; i < bitmap_size; i += bitmap->format->BytesPerPixel)
 		{
-			pixels[i] = 80;              //b
-			pixels[i + 1] = 80;          //g 
-			pixels[i + 2] = 80;          //r
+			//BGR
+			pixels[i] = 80;
+			pixels[i + 1] = 80;
+			pixels[i + 2] = 80;
 		}
 
 		SDL_UpdateWindowSurface(window);
